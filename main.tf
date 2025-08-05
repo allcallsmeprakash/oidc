3cc9252d2c4b556a158f428d9c714c77e1e9fdf6
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-05 12:54:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
