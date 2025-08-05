e5ede8b67344a67d9f16f09b5a52969668d22185
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 02:44:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,8 +29,7 @@ resource "aws_iam_role" "github_oidc_role" {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
           },
           StringLike = {
-            "token.actions.githubusercontent.com:sub": "repo:allcallsmeprakash/usecase*:*",
-            "token.actions.githubusercontent.com:sub": "repo:allcallsmeprakash/oktaapp:*"
+            "token.actions.githubusercontent.com:sub": "repo:allcallsmeprakash/*:*"
           }
         }
       }
